17c7b092fe7cc213824d59c4ce19d51b330908df
Hanay Can <hanakamer@gmail.com>
2022-03-16 09:41:19 +0200

    fix the path after renaming the custom hook

--- src/contexts/SavedGridContext.js
+++ src/contexts/SavedGridContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext } from 'react';
-import { useLocalStorageState } from '../hooks/hooks';
+import { useLocalStorageState } from '../state/useLocalStorage';
 import PropTypes from 'prop-types';
 
 const SavedGridContext = createContext();
