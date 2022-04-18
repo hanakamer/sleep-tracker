ac255e3872d586a698dd7461df7c79dd227bb677
Hanay Can <hanakamer@gmail.com>
2022-04-18 16:59:28 +0300

    radio buttons centered

--- src/components/Day/DayView.js
+++ src/components/Day/DayView.js
@@ -3,6 +3,28 @@ import PropTypes from 'prop-types';
 import { SummaryCell } from '../SummaryCell';
 import Styles from './Day.module.css';
 function DayView({ data }) {
+  console.log(data);
+
+  function clusterCells(cells) {
+    let currentMode = '';
+    let currentID;
+    const clusteredCells = cells.reduce((clusteredCells = {}, current, i) => {
+      if (currentMode === current.mode) {
+        clusteredCells[`${currentMode}_${currentID}`].push(current);
+      } else {
+        currentMode = current.mode;
+        currentID = i;
+        clusteredCells[`${current.mode}_${i}`] = [];
+        clusteredCells[`${current.mode}_${i}`].push(current);
+      }
+
+      return clusteredCells;
+    });
+    console.log(clusteredCells);
+
+    return clusteredCells;
+  }
+  clusterCells(data);
   return (
     <div className={Styles.dayViewContainer}>
       {data.map((cell) => {
